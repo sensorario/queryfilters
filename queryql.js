aef292730d491fef7fe6dc49e9f71363e75aebd2
Simone Gentili <sensorario@gmail.com>
2017-12-12 04:32:18 +0100

    add filters and rel to querystring

--- queryql.js
+++ queryql.js
@@ -1,14 +1,57 @@
 function QueryQl() {
-    return {
-        allowed: function () {
-            return [
-                'filtering',
-                'filtering_or',
-                'printing',
-                'rel',
-                'sorting',
-            ];
+    const relation = 1;    
+
+    this.filters = [];
+    this.rel = [];
+
+    this.applyFilter = function (filter) {
+        this.filters.push(filter);
+    };
+
+    this.getRels = function () {
+        this.rel = [];
+        for (f in this.filters) {
+            var filter = this.filters[f];
+            if (this.containsRelations(filter)) {
+                filterRelation = filter.field.split('.')[relation];
+                this.rel.push(filterRelation);
+            }
         }
+        return this.rel;
+    };
+
+    this.getQueryString = function () {
+        var qs = '';
+        for (f in this.filters) {
+            var filter = this.filters[f];
+            if (this.containsRelations(filter)) {
+                filterRelation = filter.field.split('.')[relation];
+                qs += 'rel=' + filterRelation;
+            }
+
+            if (qs != '') {
+                qs += '&';
+            }
+
+            qs += 'filtering['
+                + filter.field
+                + ']='
+                + filter.value
+            ;
+        }
+        return qs;
+    };
+
+    this.getFilters = function () {
+        var filters = [];
+        for (f in this.filters) {
+            filters.push(this.filters[f].field);
+        }
+        return filters;
+    };
+
+    this.containsRelations = function (filter) { 
+        return filter.field.indexOf('_embedded') !== -1;
     };
 }
 
--- queryql.test.js
+++ queryql.test.js
@@ -1,13 +1,54 @@
 const QueryQl = require('./queryql');
 
-test('allowed filters', () => {
-    var queryql = new QueryQl();
-
-    expect(queryql.allowed()).toEqual([
-        'filtering',
-        'filtering_or',
-        'printing',
-        'rel',
-        'sorting',
+test('expose a list of filters', () => {
+    var queryQl = new QueryQl()
+    queryQl.applyFilter({
+        field: 'id',
+        value: 42
+    });
+    expect(queryQl.getFilters()).toEqual([
+        'id'
     ]);
+});
+
+test('check if filter contains relations', () => {
+    var queryQl = new QueryQl()
+    expect(queryQl.containsRelations({
+        field: 'id',
+        value: 42
+    })).toBe(false);
+
+    expect(queryQl.containsRelations({
+        field: '_embedded.foo.bar',
+        value: 42
+    })).toBe(true);
+});
+
+test('build a query string with filtes', () => {
+    var queryQl = new QueryQl()
+    queryQl.applyFilter({
+        field: 'id',
+        value: 42
+    });
+    expect(queryQl.getQueryString()).toEqual('filtering[id]=42');
+});
+
+test('extract rel from filter', () => {
+    var queryQl = new QueryQl()
+    queryQl.applyFilter({
+        field: '_embedded.fizz.bar',
+        value: 666
+    });
+    expect(queryQl.getRels()).toEqual(['fizz']);
+});
+
+test('add rel with embedded filter', () => {
+    var queryQl = new QueryQl()
+    queryQl.applyFilter({
+        field: '_embedded.foo.bar',
+        value: 666
+    });
+    expect(queryQl.getQueryString()).toEqual(
+        'rel=foo&filtering[_embedded.foo.bar]=666'
+    );
 });

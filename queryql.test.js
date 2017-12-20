27bb8ef1cc516b07e3e5f92dfa74ef5366efdda7
Simone Gentili <sensorario@gmail.com>
2017-12-20 16:42:40 +0100

    remove few lines

--- queryql.test.js
+++ queryql.test.js
@@ -2,66 +2,38 @@ const QueryQl = require('./queryql');
 
 test('expose a list of filters', () => {
     var queryQl = new QueryQl()
-    queryQl.applyFilter({
-        field: 'id',
-        value: 42
-    });
-    expect(queryQl.getFilters()).toEqual([
-        'id'
-    ]);
+    queryQl.applyFilter({ field: 'id', value: 42 });
+    expect(queryQl.getFilters()).toEqual([ 'id' ]);
 });
 
 test('check if filter contains relations', () => {
     var queryQl = new QueryQl()
-    expect(queryQl.containsRelations({
-        field: 'id',
-        value: 42
-    })).toBe(false);
-
-    expect(queryQl.containsRelations({
-        field: '_embedded.foo.bar',
-        value: 42
-    })).toBe(true);
+    expect(queryQl.containsRelations({ field: 'id', value: 42 })).toBe(false);
+    expect(queryQl.containsRelations({ field: '_embedded.foo.bar', value: 42 })).toBe(true);
 });
 
 test('build a query string with filtes', () => {
     var queryQl = new QueryQl()
-    queryQl.applyFilter({
-        field: 'id',
-        value: 42
-    });
+    queryQl.applyFilter({ field: 'id', value: 42 });
     expect(queryQl.getQueryString()).toEqual('filtering[id]=42');
 });
 
 test('extract rel from filter', () => {
     var queryQl = new QueryQl()
-    queryQl.applyFilter({
-        field: '_embedded.fizz.bar',
-        value: 666
-    });
+    queryQl.applyFilter({ field: '_embedded.fizz.bar', value: 666 });
     expect(queryQl.getRels()).toEqual(['fizz']);
 });
 
 test('add rel with embedded filter', () => {
     var queryQl = new QueryQl()
-    queryQl.applyFilter({
-        field: '_embedded.foo.bar',
-        value: 666
-    });
-    expect(queryQl.getQueryString()).toEqual(
-        'rel=foo&filtering[_embedded.foo.bar]=666'
-    );
+    queryQl.applyFilter({ field: '_embedded.foo.bar', value: 666 });
+    expect(queryQl.getQueryString()).toEqual('rel=foo&filtering[_embedded.foo.bar]=666');
 });
 
 test('add rel with embedded filter even if have more levels', () => {
     var queryQl = new QueryQl()
-    queryQl.applyFilter({
-        field: '_embedded.foo.bar.field',
-        value: 666
-    });
-    expect(queryQl.getQueryString()).toEqual(
-        'rel=foo,bar&filtering[_embedded.foo.bar.field]=666'
-    );
+    queryQl.applyFilter({ field: '_embedded.foo.bar.field', value: 666 });
+    expect(queryQl.getQueryString()).toEqual('rel=foo,bar&filtering[_embedded.foo.bar.field]=666');
 });
 
 test('add all rel from each filter', () => {
